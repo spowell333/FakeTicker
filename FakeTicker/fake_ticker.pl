bc83d798b6b473e0e6bad7811ebb56ace77905c1
spowell333 <thirtythreeandathird@gmail.com>
2009-11-23 05:57:16 -0500

    work on randomiser

--- FakeTicker/fake_ticker.pl
+++ FakeTicker/fake_ticker.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
-use warnings;
+#use warnings;
 use Finance::Quote;
 use Data::Dumper;
 use BerkeleyDB;
@@ -9,6 +9,7 @@ use threads;
 use threads::shared;
 use Thread::Semaphore;
 use Thread::Queue;
+use XML::Simple;
 
 
 my $SEPARATOR = qq{\x1C}; 
@@ -19,19 +20,14 @@ my @tickers       = refresh_tickers();
 my $counter_semaphore = Thread::Semaphore->new();
 my $counter : shared  = 200 ;
 
-my %quote_data = (
-					'ask' => {},
-					'bid' => {},
-                 );	
-	
-
-my $quote_queue = Thread::Queue->new(); 
+my $quote_queue      = Thread::Queue->new();
+my $randomiser_queue =  Thread::Queue->new();
 
 my $quote_loader = threads->new(\&quote_loader, $quote_service, $quote_queue, @tickers);
-my $randomiser   = threads->new(\&quote_randomier, $quote_queue);
+my $collector    = threads->new(\&quote_collector, $quote_queue, $randomiser_queue);
 
 $quote_loader->join();
-$randomiser->join();
+$collector->join();
 
 sub quote_loader{
 	my ($quote_service, $queue, @tickers) = @_;
@@ -39,53 +35,99 @@ sub quote_loader{
 	while (can_run()) {
 		print 'Quoter can run: ' . $rep_count++  .qq{\n};
 		my %info = $quote_service->fetch("usa", @tickers);
-		
-		while (my ($k, $v) =each(%info)) { 
+		ATTRIBUTE: while (my ($k, $v) =each(%info)) { 
 			if ($k =~/ask/ or $k =~/bid/) {
+				next ATTRIBUTE if (!defined($v));
+				print $k ,' ',$v,qq{\n};
 				my @price : shared = ($k, $v);
 				$queue->enqueue(\@price);
+				
 				
 			} 
 		}
 		
-		#print Dumper $queue;
 		sleep 60;
 	}
 	
 }
 
-sub quote_randomier {
-	my ($queue) = @_;
-	my $recent_bids = {};
-	my $recent_asks = {};
+sub quote_collector {
+	my ($queue, $randomiser_queue) = @_;
+    my %quote_data  = ();
+    my %threads = ();
+    my %queues = ();
+                 
 	my $rep_count = 0 ;
 	while(my $price_ref = $queue->dequeue()) {
 		last unless (can_run());
+		
 		my ($ticker_and_type, $price)= @{$price_ref};
 		
 		my ($ticker, $type) = split(/$SEPARATOR/,$ticker_and_type, 2);
 		
+		my $most_recent_price = $quote_data{$ticker}->{$type}->[0];
+		my $prices = $quote_data{$ticker}->{$type};
+		unshift(@{$prices}, $price);
 		
-		UPDATE_PRICES: {
-			my $asks = $quote_data{$type}->{$ticker};
-			if (defined($asks)) { 
-				unshift(@{$asks}, $price);
-			} else {
-				$quote_data{$type}->{$ticker} = [$price];
+		my $queue = $queues{$ticker}->{$type}->[0];
+		if(!defined($queue)) {
+			$queue = Thread::Queue->new();
+			$queues{$ticker}->{$type}->[0] = $queue;
+			my $new_thread = threads->new(\&quote_randomiser, $ticker, $type, $queue);
+			$new_thread->detach();
 				
-			} 
-		 }
+		}
 		
-		print 'randomiser thread: ' .qq{\n};
-		print Dumper \%quote_data;
-	
-
+		my $shared_price : shared = $price; 
+		$queue->enqueue($shared_price);
 	}
+}
+
+sub quote_randomiser{
+	my $ticker = shift;
+	my $type   = shift;
+	my $q      = shift;
 	
+	print 'randomiser for ' . $ticker . ' / ' . $type . qq{\n}; 
+		
+	my @all_quotes = ();
+	while (1) {
+		my $fresh_quote = $q->dequeue_nb();
+		if (defined($fresh_quote)) { 
+			unshift(@all_quotes, $fresh_quote);
+		}
+		if ( 0 == scalar(@all_quotes)) { 
+			$fresh_quote = $q->dequeue();
+			unshift(@all_quotes, $fresh_quote);
+		}
+		
+		
+		my $data_points = scalar(@all_quotes);
+		$data_points = ($data_points > 20) ? 20 :$data_points;
+		 
+		my $mean_ask = 0 ; 
+		
+		print "Data points " . $data_points .qq{\n};
+		for(my $i=0 ; $i < $data_points ; $i++) { 
+			$mean_ask += $all_quotes[$i];
+		}
+		
+		$mean_ask /= ($data_points * 1.0);
+		print 'mean '  .  $type . ' for ' . $ticker . ' is ' .$mean_ask .qq{\n};
+		
+		my $msg = XMLout({
+			'TICKER' => $ticker,
+			'TYPE'   => $type,
+			'PRICE'  => $mean_ask,
+		});
+		print $msg;
+		sleep 3;
+	}
 }
 
 sub refresh_tickers {
-	return qw{GS GOOG MKTX BAC BARC.L};
+	return qw{GS GOOG MKTX BAC BARC.L GM};
+	#return qw{GOOG};
 }
 
 sub get_ask{
