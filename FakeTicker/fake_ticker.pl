067f32bd81ea00ae036287445b01e27f5c28a7f2
spowell333 <thirtythreeandathird@gmail.com>
2009-11-25 06:22:53 -0500

    added publishers

--- FakeTicker/fake_ticker.pl
+++ FakeTicker/fake_ticker.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl -w
 use strict;
-#use warnings;
 use Finance::Quote;
 use Data::Dumper;
-use BerkeleyDB;
 use threads;
 
 use threads::shared;
 use Thread::Semaphore;
 use Thread::Queue;
 use XML::Simple;
+use JSON;
+use IO::Socket::INET;
+
 
 
 my $SEPARATOR = qq{\x1C}; 
+my $HANGUP    = qq{\x04};
 
 my $quote_service = Finance::Quote->new;
 my @tickers       = refresh_tickers();
@@ -50,7 +52,6 @@ sub quote_loader{
 
 sub quote_collector {
 	my ($queue, $randomiser_queue) = @_;
-    my %quote_data  = ();
     my %queues = ();
                  
 	my $rep_count = 0 ;
@@ -58,18 +59,13 @@ sub quote_collector {
 		last unless (can_run());
 		
 		my ($ticker_and_type, $price)= @{$price_ref};
-		
 		my ($ticker, $type) = split(/$SEPARATOR/,$ticker_and_type, 2);
-		
-		my $most_recent_price = $quote_data{$ticker}->{$type}->[0];
-		my $prices = $quote_data{$ticker}->{$type};
-		unshift(@{$prices}, $price);
 		
 		my $queue = $queues{$ticker}->{$type}->[0];
 		if(!defined($queue)) {
 			$queue = Thread::Queue->new();
 			$queues{$ticker}->{$type}->[0] = $queue;
-			my $new_thread = threads->new(\&quote_randomiser, $ticker, $type, $queue);
+			my $new_thread = threads->new(\&quote_randomiser, $ticker, $type, $queue, \&pulish_as_json);
 			$new_thread->detach();
 				
 		}
@@ -80,45 +76,139 @@ sub quote_collector {
 }
 
 sub quote_randomiser{
-	my $ticker = shift;
-	my $type   = shift;
-	my $q      = shift;
+	my $ticker		= shift;
+	my $type		= shift;
+	my $q			= shift;
+	my $publisher 	= shift;
+	my $DEBUG 		= shift || 0;
 	
 	print 'randomiser for ' . $ticker . ' / ' . $type . qq{\n}; 
 		
 	my @all_quotes = ();
+	my $recent_quote = 0;
 	while (1) {
 		my $fresh_quote = $q->dequeue_nb();
 		if (defined($fresh_quote)) { 
 			unshift(@all_quotes, $fresh_quote);
+			$recent_quote = $fresh_quote;
 		}
-		
 		
 		if ( 0 == scalar(@all_quotes)) { 
 			$fresh_quote = $q->dequeue();
 			unshift(@all_quotes, $fresh_quote);
+			$recent_quote = $fresh_quote;
 		}
 		
 		my $data_points = scalar(@all_quotes);
 		$data_points = ($data_points > 20) ? 20 :$data_points;
 		 
-		my $mean_ask = 0 ; 
+		my $mean_price = 0 ; 
 		
 		for(my $i=0 ; $i < $data_points ; $i++) { 
-			$mean_ask += $all_quotes[$i];
+			$mean_price += $all_quotes[$i];
 		}
 		
-		$mean_ask /= ($data_points * 1.0);
-		print 'mean '  .  $type . ' for ' . $ticker . ' is now ' .$mean_ask .qq{\n};
+		$mean_price /= ($data_points * 1.0);
 		
-		my $msg = XMLout({
+		my $mean_price_with_jitter = apply_jitter($recent_quote, $mean_price, $DEBUG);
+		my $mean_price_with_rounding  = apply_rounding($mean_price_with_jitter, $type, $recent_quote, $DEBUG );
+		
+		print 'mean '  .  $type . ' for ' . $ticker . ' is now ' .$mean_price_with_rounding . 
+		' latest price ' . $recent_quote . ' with ' . $data_points . ' data point(s)'. qq{\n} if ($DEBUG);
+		
+		my $quote = {
 			'TICKER' => $ticker,
 			'TYPE'   => $type,
-			'PRICE'  => $mean_ask,
-		});
-		print $msg;
-		sleep 3;
+			'PRICE'  => $mean_price_with_rounding,
+		};
+		
+		$publisher->($quote);
+		
+		threads->yield();
+		sleep(2);
 	}
+}
+
+sub apply_jitter{
+	my $real_quote = shift; 
+	my $fake_quote = shift; 
+	my $DEBUG       = shift || 0;
+	
+	my $diff = $real_quote - $fake_quote;
+	print 'About to apply a difference of ' . $diff .qq{\n} if ($DEBUG);
+	print '$diff is '.(($diff>0)?'>':'<=').' 0, we should be ' . (($diff>0) ?  'BUY' : 'SELL') . 'ING' .qq{\n} if ($DEBUG);
+	
+	my $jitter = rand() - 0.5;
+	print 'About to scale $diff by ' . $jitter .qq{\n} if ($DEBUG);
+	
+	return $fake_quote + ($diff * $jitter);
+}
+
+sub pow($$) {
+	my $base  = shift;
+	my $power = shift;
+	my $answer = $base;
+	while ($power > 1) { 
+		$answer *= $base;
+		$power--;
+	}
+	return $answer;
+}
+
+sub apply_rounding{
+	my $fake_quote = shift || die;
+	my $type       = shift || die;
+	my $real_quote = shift || die;
+	my $DEBUG      = shift || 0;  
+	
+	my $decimal_places  = length($1) if ($real_quote =~/\d*?\.(\d*)\z/xms);
+    print 'decimal places ' . $decimal_places .qq{\n} 	if ($DEBUG) ;
+	
+	my $multiplier = pow(10.0,$decimal_places);
+	print '$multiplier ' . $multiplier .qq{\n} if ($DEBUG);
+	
+	my $scaled_up_price = $fake_quote * $multiplier;
+	print '$scaled_up_price ' . $scaled_up_price .qq{\n} if ($DEBUG);
+	
+	my $rounding_dir    = (($type eq 'bid')?-1:1);
+	print '$rounding_dir ' . $rounding_dir .qq{\n} if ($DEBUG);
+	
+	return int($scaled_up_price + (0.5 * $rounding_dir) ) / $multiplier;
+	
+}
+sub pulish_as_json($){
+	my $quote = shift;
+	my $json_text = to_json($quote);
+	print $json_text .qq{\n};
+	send_to_port($json_text);
+}
+
+sub publish_as_xml($) { 
+	my $quote = shift;
+	my $msg = XMLout($quote);
+	print $msg .qq{\n};
+}
+
+
+sub send_to_port($) { 
+	my $message = shift;
+	my $port    = get_server_port();
+	my $server  = get_server_address();
+
+	my $sock = IO::Socket::INET->new($server . ':' . $port ) ; 
+
+	my $optional_newline = ($message =~ /\n\z/xms) ? q{} : qq{\n} ;
+	if (defined($sock) && $sock->connected()) { 
+		$sock->send($message . $optional_newline . $HANGUP . qq{\n});
+	}
+
+}
+
+sub get_server_address(){
+	return "127.0.0.1";
+}
+sub get_server_port() { 
+	return 4200;
 }
 
 sub refresh_tickers {
